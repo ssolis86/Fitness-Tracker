e96c1910469dab4fcdf1d89601dd63a9ae48234c
Stephen Solis <ssolis8619@gmail.com>
2021-05-21 21:08:13 -0500

    setup seed.js, workout Schema

---
+++ models/workout.js
@@ -0,0 +1,41 @@
+const mongoose = require("mongoose");
+
+const Schema = mongoose.Schema;
+
+const workoutSchema = new Schema({
+    day: {
+        type: Date,
+        default: Date.now
+    },
+    exercises: [
+        {
+            type: {
+                type: String,
+                require: true
+                },
+            name: {
+                type: String,
+                required: true
+            },
+            duration: {
+                type: Number,
+                required: true
+            },
+            weight: {
+                type: Number
+            },
+            reps: {
+                type: Number
+            },
+            sets: {
+                type: Number
+            },
+            distance: {
+                type: Number
+            },
+        }
+    ]
+});
+
+const Exercise = mongoose.model('Exercise', workoutSchema);
+module.exports = Exercise;
